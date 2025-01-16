659be88368a2fe03f41baec6869e86c5c5763733
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2025-01-16 20:46:36 +0530

    Initial Commit

---
+++ AssessmentSysmind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssessmentSysmind
+//
+//  Created by Ganpat Jangir on 16/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
